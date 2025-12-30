a49bd12b891744a48b56969008252c3a121113d1
Gustavo Meza Garcia <gustavomg@google.com>
2025-12-30 00:03:48 -0600

    scroll animation

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
+  const imgHolders = gsap.utils.toArray(".img-holder");
+
+  imgHolders.forEach((imgHolder, i) => {
+    ScrollTrigger.create({
+      trigger: imgHolder,
+      start: "top top",
+      pin: true,
+      pinSpacing: false
+    });
+  });
 });
